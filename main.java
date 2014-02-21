85b8bfe06f99859359f0e6096b29315a32334faa
Markus <ket14396@spengergasse.at>
2014-02-21 10:28:02 +0100

    Signed-off-by: Bob

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main{
 	public Main(){
 		//just a empty constructor that does nothing
+		System.out.println("This is a whole new Branch");
 		System.out.println("But I will push this");
 	}
 }
