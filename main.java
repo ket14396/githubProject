b35e3bd2aa9c1bfe0f8bc332bd7f9d7f03a33f7d
Markus <ket14396@spengergasse.at>
2014-02-21 10:33:50 +0100

    Changed a line to force a conflict

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main{
 	public Main(){
 		//just a empty constructor that does nothing
-		System.out.println("But I will push this");
+		System.out.println("But I will push this now");
 		System.out.println("Fixed bug that never was there in the first place :P");
 	}
 }
