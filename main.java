9c0a3451997ea1ad6f6f6d81ea63eefa9aa311e7
Markus <ket14396@spengergasse.at>
2014-02-21 10:32:25 +0100

    bug fix for main

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main{
 	public Main(){
 		//just a empty constructor that does nothing
 		System.out.println("But I will push this");
+		System.out.println("Fixed bug that never was there in the first place :P");
 	}
 }
