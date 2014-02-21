34b6931386c78dba1a13977b52bcaebec55fe9c7
ket14396 <ket14396@spengergasse.at>
2014-02-21 10:39:42 +0100

    Update main.java
    added line to make a fetch

--- main.java
+++ main.java
@@ -4,5 +4,6 @@ public class main{
 		System.out.println("But I will push this now");
 		System.out.println("Fixed bug that never was there in the first place :P");
 		System.out.println("This is a whole new Branch");
+		System.out.println("added that line in github to make a fetch");
 	}
 }
